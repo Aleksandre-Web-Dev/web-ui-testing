318596212440366e6514b65b184449a1793b2743
Aleksandre-Web-Dev <pitskhelauri.aleksandre@gmail.com>
2023-05-03 22:44:11 +0400

    Added: Verify User Login Functionality with invalid credentials Testcase

--- project/test/specs/loginTest.js
+++ project/test/specs/loginTest.js
@@ -1,12 +1,13 @@
 import LoginPage from "../pageObjects/loginPage.js";
 import DriverUtils from "../../../framework/utilities/driverUtils.js";
 import StringUtils from "../../../framework/utilities/stringUtils.js";
+import RandomUtils from "../../../framework/utilities/randomUtils.js";
 import testData from "../../testData/data.json" assert { type: "json" };
 import urlConfig from "../../configurations/urlConfig.json" assert { type: "json" };
 import { assert } from "chai";
 
 describe("User Login Test", async () => {
-  before("navigate to Login Page", async () => {
+  beforeEach("navigate to Login Page", async () => {
     await DriverUtils.navigateTo(urlConfig.urls.login);
   });
 
@@ -14,7 +15,6 @@ describe("User Login Test", async () => {
     await LoginPage.enterUsername(testData.users.username);
     await LoginPage.enterPassword(testData.users.password);
     await LoginPage.clickOnSubmitButton();
-
     const validationStatusText = StringUtils.sliceString(
       await LoginPage.getNotificationText(),
       null,
@@ -24,6 +24,22 @@ describe("User Login Test", async () => {
       testData.statusText.success,
       validationStatusText,
       "User Login was unsuccesful"
+    );
+  });
+
+  it("should verify login functionality with invalid username and password", async () => {
+    await LoginPage.enterUsername(RandomUtils.generateRandomString());
+    await LoginPage.enterPassword(RandomUtils.generateRandomString());
+    await LoginPage.clickOnSubmitButton();
+    const validationStatusText = StringUtils.sliceString(
+      await LoginPage.getNotificationText(),
+      null,
+      testData.statusText.invalid_username.length
+    );
+    assert.equal(
+      testData.statusText.invalid_username,
+      validationStatusText,
+      "User Login was successful"
     );
   });
 });
