a3a761e120feaaa703ae46ab560ac2312e6171db
Aleksandre-Web-Dev <pitskhelauri.aleksandre@gmail.com>
2023-06-29 22:12:48 +0400

    Added: Test for Addition/Removal of Elements

--- project/test/specs/dropDownTest.js
+++ project/test/specs/dropDownTest.js
@@ -1,39 +1,39 @@
-// import { assert } from "chai";
-// import DriverUtils from "../../../framework/utilities/driverUtils.js";
-// import StringUtils from "../../../framework/utilities/stringUtils.js";
-// import urlConfig from "../../configurations/urlConfig.json" assert { type: "json" };
-// import DropDownPage from "../pageObjects/dropDownPage.js";
+import { assert } from "chai";
+import DriverUtils from "../../../framework/utilities/driverUtils.js";
+import StringUtils from "../../../framework/utilities/stringUtils.js";
+import urlConfig from "../../configurations/urlConfig.json" assert { type: "json" };
+import DropDownPage from "../pageObjects/dropDownPage.js";
 
-// describe("Testing Dropdown menu", async () => {
-//   before("Navigate to Dropdown page", async function () {
-//     await DriverUtils.navigateTo(urlConfig.urls.pages.dropdown);
-//     assert.isTrue(
-//       await DropDownPage.isPageDisplayed(),
-//       "Dropdown page is not displayed"
-//     );
-//   });
+describe("Testing Dropdown menu", async () => {
+  before("Navigate to Dropdown page", async function () {
+    await DriverUtils.navigateTo(urlConfig.urls.pages.dropdown);
+    assert.isTrue(
+      await DropDownPage.isPageDisplayed(),
+      "Dropdown page is not displayed"
+    );
+  });
 
-//   it("should Verify option value alignment", async () => {
-//     const list = await DropDownPage.getSelectOptionList();
-//     for (let i = 1; i < list.length; i++) {
-//       assert.equal(
-//         await DropDownPage.getOptionValue(list, i),
-//         await StringUtils.extractNumberFromString(
-//           await DropDownPage.getOptionText(list, i)
-//         ),
-//         "Option values do not align"
-//       );
-//     }
-//   });
+  it("should Verify option value alignment", async () => {
+    const list = await DropDownPage.getSelectOptionList();
+    for (let i = 1; i < list.length; i++) {
+      assert.equal(
+        await DropDownPage.getOptionValue(list, i),
+        await StringUtils.extractNumberFromString(
+          await DropDownPage.getOptionText(list, i)
+        ),
+        "Option values do not align"
+      );
+    }
+  });
 
-//   it("should Verify option selection functionality", async () => {
-//     const list = await DropDownPage.getSelectOptionList();
-//     for (let i = 1; i < list.length; i++) {
-//       await DropDownPage.clickOnOption(list, i);
-//       assert.isTrue(
-//         await DropDownPage.isOptionSelected(list, i),
-//         "Option is not selected"
-//       );
-//     }
-//   });
-// });
+  it("should Verify option selection functionality", async () => {
+    const list = await DropDownPage.getSelectOptionList();
+    for (let i = 1; i < list.length; i++) {
+      await DropDownPage.clickOnOption(list, i);
+      assert.isTrue(
+        await DropDownPage.isOptionSelected(list, i),
+        "Option is not selected"
+      );
+    }
+  });
+});
--- project/test/specs/loginTest.js
+++ project/test/specs/loginTest.js
@@ -1,67 +1,67 @@
-// import { assert } from "chai";
-// import DriverUtils from "../../../framework/utilities/driverUtils.js";
-// import DataUtils from "../../../framework/utilities/dataUtils.js";
-// import StringUtils from "../../../framework/utilities/stringUtils.js";
-// import logger from "../../../framework/utilities/logger.js";
-// import testData from "../../testData/combined.json" assert { type: "json" };
-// import urlConfig from "../../configurations/urlConfig.json" assert { type: "json" };
-// import utilConfig from "../../configurations/utilConfig.json" assert { type: "json" };
-// import LoginPage from "../pageObjects/loginPage.js";
-// import SecurePage from "../pageObjects/securePage.js";
+import { assert } from "chai";
+import DriverUtils from "../../../framework/utilities/driverUtils.js";
+import DataUtils from "../../../framework/utilities/dataUtils.js";
+import StringUtils from "../../../framework/utilities/stringUtils.js";
+import logger from "../../../framework/utilities/logger.js";
+import testData from "../../testData/combined.json" assert { type: "json" };
+import urlConfig from "../../configurations/urlConfig.json" assert { type: "json" };
+import utilConfig from "../../configurations/utilConfig.json" assert { type: "json" };
+import LoginPage from "../pageObjects/loginPage.js";
+import SecurePage from "../pageObjects/securePage.js";
 
-// describe("Login test", async () => {
-//   before("Generate test data", function () {
-//     this.userTestData = DataUtils.generateTestData();
-//   });
+describe("Login test", async () => {
+  before("Generate test data", function () {
+    this.userTestData = DataUtils.generateTestData();
+  });
 
-//   beforeEach("Navigate to Login page", async () => {
-//     await DriverUtils.navigateTo(urlConfig.urls.pages.login);
-//     assert.isTrue(await LoginPage.isPageDisplayed(), "Login Page did not open");
-//   });
+  beforeEach("Navigate to Login page", async () => {
+    await DriverUtils.navigateTo(urlConfig.urls.pages.login);
+    assert.isTrue(await LoginPage.isPageDisplayed(), "Login Page did not open");
+  });
 
-//   const arrLoop = new Array(utilConfig.default.data.length.data).fill();
-//   arrLoop.forEach((element, index) => {
-//     it("should verify login functionality with invalid username and password", async function () {
-//       for (const [fieldName, credential] of Object.entries(
-//         this.userTestData[index]
-//       )) {
-//         await LoginPage.enterUserCredential(fieldName, credential);
-//         logger.info(
-//           `Checking user account ID#${index}, Field Name: ${fieldName}, Credentials: ${credential}`
-//         );
-//       }
-//       await LoginPage.clickOnSubmitButton();
-//       const validationStatusText = await StringUtils.sliceString(
-//         await LoginPage.getNotificationText(),
-//         testData.status_text.invalid_username.length
-//       );
-//       assert.equal(
-//         testData.status_text.invalid_username,
-//         validationStatusText,
-//         "User Login was successfull"
-//       );
-//     });
-//   });
+  const arrLoop = new Array(utilConfig.default.data.length.data).fill();
+  arrLoop.forEach((element, index) => {
+    it("should verify login functionality with invalid username and password", async function () {
+      for (const [fieldName, credential] of Object.entries(
+        this.userTestData[index]
+      )) {
+        await LoginPage.enterUserCredential(fieldName, credential);
+        logger.info(
+          `Checking user account ID#${index}, Field Name: ${fieldName}, Credentials: ${credential}`
+        );
+      }
+      await LoginPage.clickOnSubmitButton();
+      const validationStatusText = await StringUtils.sliceString(
+        await LoginPage.getNotificationText(),
+        testData.status_text.invalid_username.length
+      );
+      assert.equal(
+        testData.status_text.invalid_username,
+        validationStatusText,
+        "User Login was successfull"
+      );
+    });
+  });
 
-//   it("should verify login functionality with valid username and password", async function () {
-//     for (let [fieldName, credentials] of Object.entries(testData.validUser)) {
-//       await LoginPage.enterUserCredential(fieldName, credentials);
-//     }
-//     await LoginPage.clickOnSubmitButton();
-//     const validationStatusText = await StringUtils.sliceString(
-//       await LoginPage.getNotificationText(),
+  it("should verify login functionality with valid username and password", async function () {
+    for (let [fieldName, credentials] of Object.entries(testData.validUser)) {
+      await LoginPage.enterUserCredential(fieldName, credentials);
+    }
+    await LoginPage.clickOnSubmitButton();
+    const validationStatusText = await StringUtils.sliceString(
+      await LoginPage.getNotificationText(),
 
-//       testData.status_text.success.length
-//     );
-//     assert.equal(
-//       testData.status_text.success,
-//       validationStatusText,
-//       "User Login was unsuccesful"
-//     );
-//   });
+      testData.status_text.success.length
+    );
+    assert.equal(
+      testData.status_text.success,
+      validationStatusText,
+      "User Login was unsuccesful"
+    );
+  });
 
-//   after("Logout user", async () => {
-//     await SecurePage.clickOnLogoutButton();
-//     assert.isTrue(await LoginPage.isPageDisplayed(), "Login Page did not open");
-//   });
-// });
+  after("Logout user", async () => {
+    await SecurePage.clickOnLogoutButton();
+    assert.isTrue(await LoginPage.isPageDisplayed(), "Login Page did not open");
+  });
+});
